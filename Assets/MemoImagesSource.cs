b44b9b01ce8083fd12fdd794f2f8489aab42f4ee
g.myjak <g.myjak@neurosys.com>
2021-03-04 14:46:37 +0100

    Add game content scaffold and import sprites

---
+++ Assets/MemoImagesSource.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoImagesSource : MonoBehaviour
+{
+    [SerializeField]
+    private List<Sprite> images;
+
+    public List<Sprite> Images
+    {
+        get => images;
+        set => images = value;
+    }
+}
